037b659d8525da79d04f99b399e865c26af274a5
Nanakelol <contact@le-pixel.com>
2024-01-22 13:21:36 +0100

    Retrait des parties du codes liées à feur

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ import keys from './varprod.json' assert {type: 'json'};
 console.log(keys,keys["APIKEY"])
 const rest = new REST({ version: '10' }).setToken(keys["APIKEY"]);
 import fetch from 'node-fetch'; // Importez le module fetch
-
 import histoires  from './histoirennk.json' assert {type: 'json'};
 const fs = import('fs');
 const JSON_FILE = 'histoirennk.json';
-const userIDFeminin = ['346737432869470219','349898990088749056','280804408457953280','776137866636034079','152527402252238848','280804408457953280','270279322755923969'];
 const client = new Client({
 	intents: [ // Toutes les autorisations du BOT
 		GatewayIntentBits.Guilds,
@@ -24,22 +22,6 @@ const commands = [
     description: 'Obtenir une histoire aléatoire',
   },
 ];
-/*const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-  {
-    name: 'papillon',
-    description: 'Replies with de lumière!',
-  },
-];
-
-
-function getLastWord(str) {
-  return (str.toLowerCase().match(/(\w+)\W*$/) || [])[1];
-} */
-
 try {
   console.log('Started refreshing application (/) commands.');
 
@@ -76,75 +58,7 @@ client.on('interactionCreate', async (interaction) => {
     }
   }
 });
-
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return; // ignore les messages des bots
-  const auteurid = message.author.id;
-  // récupère le contenu en minuscule (jsp pourquoi mais les gens font ça, peut être pour éviter un tableau avec plusieurs valeurs du même mot)
-  const content = message.content.toLowerCase();
-  // regarde si le message se termine par "quoi" (en excluant la ponctuation finale)
-  // Créez un tableau de réponses possibles
-  const reponsesAuteurM = ['feur', 'bah juste feur en fait','bah feur écoute','feur je crois ?','heu feur ?','juste feur mon pote'];
-  const reponsesAuteurF = ['feur', 'bah juste feur en fait','bah feur écoute','feur je crois ?','heu feur ?','juste feur ma pote'];
-  const reponseAleatoireM = reponsesAuteurM[Math.floor(Math.random() * reponsesAuteurM.length)];
-  const reponseAleatoireF= reponsesAuteurF[Math.floor(Math.random() * reponsesAuteurF.length)];
-  if (content.match(/\bquoi\s*[.!?]*\s*$/)) {
-    // répond feur
-    if(userIDFeminin.includes(auteurid)) {
-    message.channel.send(reponseAleatoireF);
-    }
-    else {
-      message.channel.send(reponseAleatoireM);  
-    }
-   }  
-});
-
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return; // ignore les messages des bots
-  // récupère le contenu en minuscule (jsp pourquoi mais les gens font ça, peut être pour éviter un tableau avec plusieurs valeurs du même mot)
-  const content = message.content.toLowerCase();
-  // regarde si le message se termine par "quoicoubé" (en excluant la ponctuation finale)
-  if (content.match(/quoicoubé\s*[.!?]*\s*$/)) {
-    // Répondez avec t'as les cramptés?
-    message.channel.send('T\'AS LES CRAMPTÉS?');
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return; // ignore les messages des bots
-  const auteurid = message.author.id;
-  // récupère le contenu en minuscule (jsp pourquoi mais les gens font ça, peut être pour éviter un tableau avec plusieurs valeurs du même mot)
-  const content = message.content.toLowerCase();
-  // regarde si le message se termine par "quoi" (en excluant la ponctuation finale)
-  // Créez un tableau de réponses possibles
-  const reponsesAuteurM = ['pour feur je crois mec',"bah clairement pour feur mec"];
-  const reponsesAuteurF = ['pour feur je crois meuf',"bah clairement pour feur meuf"];
-  const reponseAleatoireM = reponsesAuteurM[Math.floor(Math.random() * reponsesAuteurM.length)];
-  const reponseAleatoireF= reponsesAuteurF[Math.floor(Math.random() * reponsesAuteurF.length)];
-  if (content.match(/pourquoi\s*[.!?]*\s*$/)) {
-    // répond feur
-    if(userIDFeminin.includes(auteurid)) {
-    message.channel.send(reponseAleatoireF);
-    }
-    else {
-      message.channel.send(reponseAleatoireM);  
-    }
-   }  
-});
-
 client.on('ready', () => {
   console.log(`Connecté sous ${client.user.tag}!`);
 });
-
-/*client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-  if (interaction.commandName === 'papillon') {
-    await interaction.reply('de lumière !');
-  }
-});*/
-
 client.login(keys["APIKEY"]);
